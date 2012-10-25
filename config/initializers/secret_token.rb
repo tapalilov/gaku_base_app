3e5e110580de9b2f6ee87db7aa4d9a4f3c0e86b9
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-25 14:11:08 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GakuBaseApp::Application.config.secret_token = 'c712f8964681d471e2c7a06690ba8fe4c8df53a2faaf0e495737589b3196b3cd7429e815edce055377a1e8f8a27a7593fb03504e50a5ee073825cf713286f50a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GakuBaseApp::Application.config.session_store :cookie_store, key: '_gaku_base_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GakuBaseApp::Application.config.session_store :active_record_store
